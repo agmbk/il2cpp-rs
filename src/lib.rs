ac1a57c6c8eceaa79e598567da9cb1d4821c5c87
AGMBK <109393672+agmbk@users.noreply.github.com>
2025-11-18 20:50:32 +0300

    fix: ref and non-null ref implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,76 +1,118 @@
-extern crate core;
+//! Safe Rust bindings for IL2CPP internals
 
 mod array;
 mod class;
 mod string;
 
+/// Raw IL2CPP bindings
 pub mod sys {
     pub use il2cpp_sys_rs::*;
 }
 
 pub use array::*;
 pub use class::*;
+use std::any::type_name;
 use std::cmp::Ordering;
 use std::hash::{Hash, Hasher};
 use std::marker::PhantomData;
+use std::ptr::NonNull;
+use std::{fmt, ptr};
 pub use string::*;
 
+/// Nullable managed IL2CPP reference
 #[repr(transparent)]
-#[derive(Clone, Copy)]
 pub struct Ref<T, G> {
     ptr: *mut T,
     _marker: PhantomData<G>,
 }
 
+/// Non-null managed IL2CPP reference
 #[repr(transparent)]
-#[derive(Clone, Copy)]
 pub struct NonNullRef<T, G> {
-    ptr: ::core::ptr::NonNull<T>,
+    ptr: NonNull<T>,
     _marker: PhantomData<G>,
 }
 
 impl<T, G> Ref<T, G> {
+    /// Creates a new reference from a raw pointer
+    ///
+    /// # Arguments
+    ///
+    /// * `ptr` - Raw pointer
     #[inline]
-    pub const fn from_ptr(ptr: *mut T) -> Self {
+    pub const fn new(ptr: *mut T) -> Self {
         Self {
             ptr,
             _marker: PhantomData,
         }
     }
 
+    /// Creates a null reference
+    #[inline]
+    pub const fn null() -> Self {
+        Self {
+            ptr: ptr::null_mut(),
+            _marker: PhantomData,
+        }
+    }
+
+    /// Returns the inner raw pointer
     #[inline]
     pub const fn as_ptr(self) -> *mut T {
         self.ptr
     }
 
+    /// Whether the reference is null
     #[inline]
     pub const fn is_null(self) -> bool {
         self.ptr.is_null()
     }
 
+    /// Immutable reference to the value if not null
     #[inline]
     pub const fn as_ref<'a>(self) -> Option<&'a T> {
         unsafe { self.ptr.as_ref() }
     }
 
+    /// Mutable reference to the value if not null
     #[inline]
     pub const fn as_mut<'a>(self) -> Option<&'a mut T> {
         unsafe { self.ptr.as_mut() }
     }
 
+    /// Immutable reference
+    ///
+    /// # Panics
+    ///
+    /// Panics if the value is null
+    ///
+    /// # Arguments
+    ///
+    /// * `msg` - Panic message
+    #[track_caller]
     #[inline]
     pub const fn expect<'a>(self, msg: &str) -> &'a T {
         self.as_ref().expect(msg)
     }
 
+    /// Mutable reference
+    ///
+    /// # Arguments
+    ///
+    /// * `msg` - Panic message
+    ///
+    /// # Panics
+    ///
+    /// Panics if the value is null
     #[inline]
     pub const fn expect_mut<'a>(self, msg: &str) -> &'a mut T {
         self.as_mut().expect(msg)
     }
 
+    /// Converts to a non-null reference if the value is not null
     #[inline]
     pub const fn non_null(self) -> Option<NonNullRef<T, G>> {
-        if let Some(ptr) = ::core::ptr::NonNull::new(self.ptr) {
+        if let Some(ptr) = NonNull::new(self.ptr) {
             Some(NonNullRef {
                 ptr,
                 _marker: self._marker,
@@ -80,11 +122,25 @@ impl<T, G> Ref<T, G> {
         }
     }
 
+    /// Converts to a non-null reference
+    ///
+    /// # Panics
+    ///
+    /// Panics when called on a null reference
+    #[track_caller]
     #[inline]
     pub const fn unwrap_non_null(self) -> NonNullRef<T, G> {
         self.non_null().unwrap()
     }
 }
+
+impl<T, G> Clone for Ref<T, G> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<T, G> Copy for Ref<T, G> {}
 
 impl<T, G> PartialEq for Ref<T, G> {
     fn eq(&self, other: &Self) -> bool {
@@ -112,19 +168,77 @@ impl<T, G> Hash for Ref<T, G> {
     }
 }
 
+impl<T, G> From<*mut T> for Ref<T, G> {
+    #[inline]
+    fn from(value: *mut T) -> Self {
+        Self::new(value)
+    }
+}
+
+impl<T, G> From<NonNullRef<T, G>> for Ref<T, G> {
+    #[inline]
+    fn from(value: NonNullRef<T, G>) -> Self {
+        Self::new(value.as_ptr())
+    }
+}
+
+impl<T, G> Default for Ref<T, G> {
+    fn default() -> Self {
+        Self::null()
+    }
+}
+
+impl<T, G> fmt::Debug for Ref<T, G> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple(type_name::<Self>()).field(&self.ptr).finish()
+    }
+}
+
 impl<T, G> NonNullRef<T, G> {
+    /// Creates a non-null reference from a raw pointer
+    ///
+    /// # Arguments
+    ///
+    /// * `ptr` - Raw pointer
+    ///
+    /// # Returns
+    ///
+    /// Non-null reference if `ptr` is not null, otherwise `None`
+    #[inline]
+    pub const fn from_ptr(ptr: *mut T) -> Option<Self> {
+        if let Some(ptr) = NonNull::new(ptr) {
+            Some(Self {
+                ptr,
+                _marker: PhantomData,
+            })
+        } else {
+            None
+        }
+    }
+
+    /// Returns the raw non-null pointer
     #[inline]
     pub const fn as_ptr(self) -> *mut T {
         self.ptr.as_ptr()
     }
 
+    /// Immutable reference to the value
     #[inline]
     pub const fn as_ref<'a>(self) -> &'a T {
         unsafe { self.ptr.as_ref() }
     }
 
+    /// Mutable reference to the value
     #[inline]
     pub const fn as_mut<'a>(mut self) -> &'a mut T {
         unsafe { self.ptr.as_mut() }
     }
 }
+
+impl<T, G> Clone for NonNullRef<T, G> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<T, G> Copy for NonNullRef<T, G> {}
